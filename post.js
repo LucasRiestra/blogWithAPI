f765f8c884bd9c77710230c0ebee200dd06a01cd
Lucas Riestra <lucas.riestra94@gmail.com>
2023-07-28 11:44:24 +0200

    with first modal

--- post.js
+++ post.js
@@ -1,6 +1,18 @@
 const urlPost1 = "https://jsonplaceholder.typicode.com/posts/";
+const urlUsers = "https://jsonplaceholder.typicode.com/users/";
+
 const postList1 = document.querySelector("#postList1");
 const postList2 = document.querySelector("#postList2");
+
+const myInput = document.getElementById('myInput')
+
+let usersData;
+
+fetch(urlUsers)
+  .then((response) => response.json())
+  .then((data) => {
+    usersData = data;
+  });
 
 fetch(urlPost1)
   .then((response) => response.json())
@@ -8,9 +20,24 @@ fetch(urlPost1)
     const arraysPost = data.slice(0, 8);
     arraysPost.forEach((post1) => {
       const li = document.createElement("li");
-
       const titleElement = document.createElement("h3");
       titleElement.textContent = post1.title;
+      
+      titleElement.addEventListener("click", () => {
+        document.getElementById("postModalTitle").textContent = post1.title;
+        document.getElementById("postModalBody").textContent = post1.body;
+
+    
+        const user = usersData.find((user) => user.id === post1.userId);
+        if (user) {
+          document.getElementById("postModalUserName").textContent = "Author: " + user.name;
+          document.getElementById("postModalUserEmail").textContent = "Email: " + user.email;
+        } 
+      });
+
+
+      titleElement.setAttribute("data-bs-toggle", "modal");
+      titleElement.setAttribute("data-bs-target", "#postModal");
 
       li.appendChild(titleElement);
       postList1.appendChild(li);
@@ -27,21 +54,31 @@ function loadMorePosts() {
   const urlPost1 = `https://jsonplaceholder.typicode.com/posts/?_start=${loadedPosts}&_limit=${postsPerPage}`;
 
   fetch(urlPost1)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((post) => {
-        const li = document.createElement("li");
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((post1) => {
+      const li = document.createElement("li");
 
-        const titleElement = document.createElement("h3");
-        titleElement.textContent = post.title;
+      const titleElement = document.createElement("h3");
+      titleElement.textContent = post1.title;
 
-        li.appendChild(titleElement);
-        postList2.appendChild(li);
 
-        const hrElement = document.createElement("hr");
-        postList2.appendChild(hrElement);
+      titleElement.setAttribute("data-bs-toggle", "modal");
+      titleElement.setAttribute("data-bs-target", "#postModal");
+      titleElement.addEventListener("click", () => {
+        
+        document.getElementById("postModalTitle").textContent = post1.title;
+        document.getElementById("postModalBody").textContent = post1.body;
+        document.getElementById("postModalUserName").textContent = "Author: " + post1.id;
+        document.getElementById("postModalUserEmail").textContent = "Email: " + post1.id;
       });
 
-      loadedPosts += 8;
+
+      li.appendChild(titleElement);
+      postList1.appendChild(li);
+
+      const hrElement = document.createElement("hr");
+      postList1.appendChild(hrElement);
     });
+  });
 };
